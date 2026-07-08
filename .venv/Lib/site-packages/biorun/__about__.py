8ad2c7ccf48d74b1b70816313818316d5f8f6f02
Harshit-singh15 <harshitsunita2006@gmail.com>
2026-07-08 19:47:45 +0530

    Add requirement.txt file

---
+++ .venv/Lib/site-packages/biorun/__about__.py
@@ -0,0 +1,12 @@
+# SPDX-FileCopyrightText: 2024-present Istvan Albert <istvan.albert@gmail.com>
+#
+# SPDX-License-Identifier: MIT
+__version__ = "1.8.3"
+
+"""
+What's new:
+
+1.8.3 - fixes deprecation warnings in Biopython
+1.8.2 - deleted due to a bug in the release process
+1.8.1 - fixes GFF download from NCBI
+"""
